446ef31695ca49a85c0f429168099e07276d4eb1
Jared Forsyth <jared@jaredforsyth.com>
2013-06-06 01:22:38 -0600

    angularize

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 
-var template = require('./template');
+var template = require('./template')
+  , angular = require('angularjs');
 
-var ng = {
-  deps: {},
-  directive: function(){
+angular.module('tags', [])
+  .directive(function(){
     return {
-    scope: {},
-    replace: true,
-    restrict: 'A',
-    template: template,
-    link: function (scope, element, attrs) {
-      var name = attrs.note;
-      var localName = 'tags';
-      scope.$parent.$watch(name, function(value) {
-        scope[localName] = value;
-      });
-      scope.$watch(localName, function(value) {
-        scope.$parent[name] = value;
-      });
-    }
-  };
-  }
-};
+      scope: {},
+      replace: true,
+      restrict: 'A',
+      template: template,
+      link: function (scope, element, attrs) {
+        var name = attrs.note;
+        var localName = 'tags';
+        scope.$parent.$watch(name, function(value) {
+          scope[localName] = value;
+        });
+        scope.$watch(localName, function(value) {
+          scope.$parent[name] = value;
+        });
+      }
+    };
+  });
 
-module.exports = ng;
 
